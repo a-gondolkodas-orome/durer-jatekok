3449d1c5fd51764f57aa0cad2fef279ab343c590
Ildiko Czeller <czeildi@gmail.com>
2021-01-23 16:35:00 +0100

    refactor css and fix playing with Szultán with temporary heck

--- 6/hunyadi-es-a-janicsarok/game.js
+++ 6/hunyadi-es-a-janicsarok/game.js
@@ -1,99 +1,56 @@
 //The game board handles all the dom interaction
 //Drawing the board and listening for click events
 var gameBoard = function(nim) {
-    var n=nim;
-    //store a reference to the container
-    var container = document.querySelector('.game').querySelector('.board');
-
-    var move = n.board();
-
+    var n = nim;
+    window.move = n.board();
+    var container = document.querySelector('.game__board');
     
-
-    //create an image node
-    var returnImg = function(num, source) {
-        //create a new image
-        var img = document.createElement("SPAN");
-        /*//set the image source
-        img.src = source;
-        //add the class so we know wich one the user is hovering over
-        //get a reference to the style object
-        //if (lastTwo) s.opacity = '0.5';*/
-        img.classList.add(num);
-        img.classList.add('dot');
-        img.classList.add(source?'redDot':'blueDot');
-        var s = img.style
-        return img;
+    var createGamePiece = function(num, source) {
+        var piece = document.createElement('span');
+        piece.classList.add(num, 'game__piece', `game__piece-${source ? 'red' : 'blue'}`);
+        return piece;
     }
-
-
-    var drawPile = function(num, source1, source2, array) {
+    
+    
+    var drawPile = function(num, array) {
         //create a document fragment once
         var frag = document.createDocumentFragment();
         //create all num images
         for (var i = 0; i < num; i = i + 1) {
-			//var lastTwo = false; 
+            //var lastTwo = false; 
 			//if (i + 2 < num) lastTwo = true;
-            var img = returnImg(i + 1, true);
+            var img = createGamePiece(i + 1, true);
             if(array[i]){
-                img = returnImg(i + 1, false);
+                img = createGamePiece(i + 1, false);
             }
-
+            
             frag.appendChild(img);
         }
         //return the fragment
         return frag;
     }
 
-    /**/
-    var hoverEvent = function() {
-        var parent = this.parentElement;
-        parent.classList[1];
-        var num = parseInt(this.parentElement.querySelectorAll('img').length, 10);
-        var rem = parseInt(this.classList[0], 10);
-		/*if (num - (rem - 1) > 2) {
-     	    info.innerHTML = 'Legfeljebb 2 ábrát választhatsz. ';						
-		} else {
-     	  if (parent.classList[1] === "pileOne") {
-     	    info.innerHTML = 'Ha ezt választod, akkor ' + (num - (rem - 1)) +
-               ' bábut változtatunk át házikóvá.';			
- 		  } else {			
-     	    info.innerHTML = 'Ha ezt választod, akkor ' + (num - (rem - 1)) +
-               ' házikót veszünk el.';			
- 		  }			
-		}
-        */  
+    var makeMove = function() {
+        if (n.status().isGameOn){
+            if (document.querySelector('.game__step-cta-text').innerHTML === "Mi jövünk." && n.state()===true) {			
+                console.error('Túl gyorsan léptél, még mi jövünk.');
+            } else if (n.state()===false) {
+                var pile =parseInt(this.parentElement.id.replace(/row\_/, ''));
+                var matches = this.classList[0]-1;
+                window.move[pile][matches]=!window.move[pile][matches];
+                drawBoard(window.move);
+            }
+        }	
+        
     }
     
-
-    var makeMove = function() {
-    	if (n.status().isGameOn){
-			if (document.querySelector('.whos').innerHTML === "Mi jövünk." && n.state()===true) {			
-			  console.error('Túl gyorsan léptél, még mi jövünk.');
-			} else if (n.state()===false) {
-	            var pile =parseInt(this.parentElement.id[1]);
-	            var matches = this.classList[0]-1;
-	            //var num = parseInt(this.parentElement.querySelectorAll('img').length, 10);
-	            //var rem = parseInt(this.classList[0], 10);
-			    //var diff = num - (rem - 1);
-			    //var numOfPileOne = document.getElementById("pileOne").querySelectorAll('img').length;
-			    //var numOfPileTwo = document.getElementById("pileTwo").querySelectorAll('img').length;
-	  		    move[pile][matches]=!move[pile][matches];
-			    drawBoard(move);
-
-	        }
-    	}	
-
-    }
-
-
     var appendEventsToBoard = function() {
         var imgs = container.getElementsByTagName('span');
         for (var i = imgs.length - 1; i >= 0; i--) {
-            imgs[i].onmouseover = hoverEvent;
             imgs[i].onclick = makeMove;
         };
     };
-
+    
     var emptyPile = function(el) {
         if (el && el.firstChild) {
             while (el.firstChild) {
@@ -101,28 +58,24 @@ var gameBoard = function(nim) {
             }
         }
     }
-
+    
     var drawBoard = function(board) {
-        move=board;
+        window.move=board;
         //loop through the board
         for (var i in board) {
             if (board.hasOwnProperty(i) && typeof i !== 'undefined') {
                 //get images
-				var frag;
-				var serverPrefix = '/wp-includes/js/janicsar/';
-			    //var serverPrefix = '';
-                
-                frag = drawPile(board[i].length, serverPrefix + 'red_circle.png', serverPrefix + 'blue_circle.png', board[i]);
+				var frag = drawPile(board[i].length, board[i]);
                 //append images to the pile
-                emptyPile(container.querySelector('#r' + i));
-			    container.querySelector('#r' + i).appendChild(frag);
+                emptyPile(container.querySelector('#row_' + i));
+			    container.querySelector('#row_' + i).appendChild(frag);
             }
         }
         appendEventsToBoard();
     };
 
     return {
-        drawBoard: drawBoard,
+        drawBoard: drawBoard
     }
 }
 
@@ -131,77 +84,68 @@ var game = (function() {
     var ai = nimAi();
     var n = nim();
     var board = gameBoard(n);
-
+    
     pubSub.sub('PLAYER_MOVE', function(move) {
         board.drawBoard(n.move(move));
         checkGame();
 		var time = Math.floor(Math.random() * 750 + 750);
         setTimeout(aiMove, time);
     });
-
+    
     var checkGame = function() {
-        document.querySelector('.whos').innerHTML = n.status().player;
-        if(document.querySelector('.whos').innerHTML === "Mi jövünk.")document.querySelector('.move').innerHTML = '';
-        else document.querySelector('.move').innerHTML = n.state()? 'Válaszd ki, hogy ma a piros vagy kék hadtestet semmisíted meg.' : 'Kattints  a korongokra és válaszd két részre a seregedet.';
+        document.querySelector('.game__step-cta-text').innerHTML = n.status().player;
+        if(document.querySelector('.game__step-cta-text').innerHTML === "Mi jövünk.")document.querySelector('.game__step-description').innerHTML = '';
+        else document.querySelector('.game__step-description').innerHTML = n.state()? 'Válaszd ki, hogy ma a piros vagy kék hadtestet semmisíted meg.' : 'Kattints  a korongokra és válaszd két részre a seregedet.';
         
         if(!n.status().isGameOn) {
-            document.querySelector('.move').innerHTML = '';
+            document.querySelector('.game__step-description').innerHTML = '';
             document.getElementById("step").style.display = 'none';
 			document.getElementById("red").style.display = 'none';
 			document.getElementById("blue").style.display = 'none';
-
+            
 			document.getElementById("startTrue").style.display = 'none';
 			document.getElementById("startFalse").style.display = 'none';
-
+            
 			document.getElementById("resetGame").style.display = 'block';
-			//var time = Math.floor(Math.random() * 1500 + 1500);
-        	//setTimeout(reset, time);
-            //document.querySelector('.gameActionButton').style.display = '';
-
         }
 		n.isBeginningOfGame = false;
     }
-
+    
     var aiMove = function() {
-        //var aiMove =  n.board();//ai.makeMove(n.board(), n.isBeginningOfGame);
         if(n.status().isGameOn){
-        	board.drawBoard(n.move(ai.makeMove(n.board(),n.state())));
+            board.drawBoard(n.move(ai.makeMove(n.board(),n.state())));
 	        checkGame();
     	}
     }
-
+    
     var step = function() {
-    	if (n.status().isGameOn)
-        if (document.querySelector('.whos').innerHTML === "Mi jövünk." && n.state()===true) {			
-		  console.error('Túl gyorsan léptél, még mi jövünk.');
-		} else if(!n.state()) pubSub.pub('PLAYER_MOVE', move);
+        if (n.status().isGameOn)
+        if (document.querySelector('.game__step-cta-text').innerHTML === "Mi jövünk." && n.state()===true) {			
+            console.error('Túl gyorsan léptél, még mi jövünk.');
+		} else if(!n.state()) pubSub.pub('PLAYER_MOVE', window.move);
     }
-
+    
     var killBlue = function() {
-    	if (n.status().isGameOn)
-    	if (document.querySelector('.whos').innerHTML === "Mi jövünk."  && n.state()===false) {			
-		  console.error('Túl gyorsan léptél, még mi jövünk.');
+        if (n.status().isGameOn)
+    	if (document.querySelector('.game__step-cta-text').innerHTML === "Mi jövünk."  && n.state()===false) {			
+            console.error('Túl gyorsan léptél, még mi jövünk.');
 		} else if(n.state()){
-    		pubSub.pub('PLAYER_MOVE', true);
+            pubSub.pub('PLAYER_MOVE', true);
     	}
     }
-
+    
     var killRed = function() {
-    	if (n.status().isGameOn)
-    	if (document.querySelector('.whos').innerHTML === "Mi jövünk."  && n.state()===false) {			
-		  console.error('Túl gyorsan léptél, még mi jövünk.');
+        if (n.status().isGameOn)
+    	if (document.querySelector('.game__step-cta-text').innerHTML === "Mi jövünk."  && n.state()===false) {			
+            console.error('Túl gyorsan léptél, még mi jövünk.');
 		} else if(n.state()){
-    		pubSub.pub('PLAYER_MOVE', false);
+            pubSub.pub('PLAYER_MOVE', false);
     	}
     }
-
+    
     var startGameAsPlayer = function(player) {
         n.startGameAsPlayer(player);
         n.isBeginningOfGame = true;
-        //document.querySelector('.whos').innerHTML = 'Te jössz.';
-
-        //document.querySelector('.gameActionButton').style.display = 'none';
-
 
 		document.getElementById("startTrue").style.display = 'none';
 		document.getElementById("startFalse").style.display = 'none';
@@ -214,8 +158,8 @@ var game = (function() {
 		document.getElementById("red").style.display = (!player)?'none':'block';
 		document.getElementById("blue").style.display = (!player)?'none':'block';
 
-        document.querySelector('.game').style.display = 'block';
-        document.querySelector('.whos').innerHTML = n.status().player;
+        document.querySelector('.game__game-area').style.display = 'block';
+        document.querySelector('.game__step-cta-text').innerHTML = n.status().player;
         board.drawBoard(n.board());
 
         checkGame();
@@ -226,8 +170,8 @@ var game = (function() {
 
     var resetGame = function() {
         board.drawBoard(n.newBoard());
-        document.querySelector('.move').innerHTML = '';
-        document.querySelector('.whos').innerHTML = 'A gombra kattintva tudod elindítani a játékot.';
+        document.querySelector('.game__step-description').innerHTML = '';
+        document.querySelector('.game__step-cta-text').innerHTML = 'A gombra kattintva tudod elindítani a játékot.';
     	document.getElementById("startTrue").style.display = 'block';
 		document.getElementById("startFalse").style.display = 'block';
 		document.getElementById("resetGame").style.display = 'none';
--- 8/kupac-ketteoszto/game.js
+++ 8/kupac-ketteoszto/game.js
@@ -2,28 +2,16 @@
 //Drawing the board and listening for click events
 var gameBoard = function(nim) {
     var n=nim;
-    //store a reference to the container
-    var container = document.querySelector('.game').querySelector('.board');
-
-    var info = document.querySelector('.move');
+    var container = document.querySelector('.game__board');
 
     var move = n.board();
-
     
 
     //create an image node
-    var returnImg = function(num) {
-        //create a new image
-        var img = document.createElement("SPAN");
-        /*//set the image source
-        img.src = source;
-        //add the class so we know wich one the user is hovering over
-        //get a reference to the style object
-        //if (lastTwo) s.opacity = '0.5';*/
-        img.classList.add(num);
-        img.classList.add('dot');
-        img.classList.add('blueDot');
-        return img;
+    var createGamePiece = function(num) {
+        var piece = document.createElement('span');
+        piece.classList.add(num, 'game__piece', 'game__piece-blue');
+        return piece;
     }
 
 
@@ -32,21 +20,14 @@ var gameBoard = function(nim) {
         var frag = document.createDocumentFragment();
         //create all num images
         for (var i = 0; i < num; i = i + 1) {
-			//var lastTwo = false; 
-			//if (i + 2 < num) lastTwo = true;
-            var img = returnImg(i + 1, true);
-
-            frag.appendChild(img);
+            frag.appendChild(createGamePiece(i + 1, true));
         }
-        //return the fragment
         return frag;
     }
 
-    /**/
     var hoverEvent = function() {
         var parent = this.parentElement;
         parent.classList[1];
-        var num = parseInt(this.parentElement.querySelectorAll('span').length, 10);
         var matches = parseInt(this.classList[0], 10)-1;
         if(matches!=0 && n.status().isGameOn)    
             for (var i = this.parentElement.children.length - 1; i >= 0; i--) {
@@ -54,25 +35,11 @@ var gameBoard = function(nim) {
                     this.parentElement.children[i].style.opacity='0.5';
                 }
             }
-		/*if (num - (rem - 1) > 2) {
-     	    info.innerHTML = 'Legfeljebb 2 ábrát választhatsz. ';						
-		} else {
-     	  if (parent.classList[1] === "pileOne") {
-     	    info.innerHTML = 'Ha ezt választod, akkor ' + (num - (rem - 1)) +
-                ' bábut változtatunk át házikóvá.';			
- 		  } else {			
-     	    info.innerHTML = 'Ha ezt választod, akkor ' + (num - (rem - 1)) +
-               ' házikót veszünk el.';			
- 		  }			
-		}
-        */  
     }
-    /**/
     
     var hoverOutEvent = function() {
         var parent = this.parentElement;
         parent.classList[1];
-        var num = parseInt(this.parentElement.querySelectorAll('span').length, 10);
         var matches = parseInt(this.classList[0], 10)-1;
         if(matches!=0)    
             for (var i = this.parentElement.children.length - 1; i >= 0; i--) {
@@ -80,33 +47,18 @@ var gameBoard = function(nim) {
                     this.parentElement.children[i].style.opacity='1';
                 }
             }
-        /*if (num - (rem - 1) > 2) {
-            info.innerHTML = 'Legfeljebb 2 ábrát választhatsz. ';                       
-        } else {
-          if (parent.classList[1] === "pileOne") {
-            info.innerHTML = 'Ha ezt választod, akkor ' + (num - (rem - 1)) +
-                ' bábut változtatunk át házikóvá.';         
-          } else {          
-            info.innerHTML = 'Ha ezt választod, akkor ' + (num - (rem - 1)) +
-               ' házikót veszünk el.';          
-          }         
-        }
-        */  
     }
     
 
     var makeMove = function() {
     	if (n.status().isGameOn){
-			if (document.querySelector('.whos').innerHTML === "Mi jövünk.") {			
+			if (document.querySelector('.game__step-description').innerHTML === "Mi jövünk.") {			
 			  console.error('Túl gyorsan léptél, még mi jövünk.');
 			} else {
-	            var pile =parseInt(this.parentElement.id[1]);
-                var matches = parseInt(this.classList[0], 10)-1;
+	            var pile =parseInt(this.parentElement.id.replace(/row_/, ''));
 	            var num = parseInt(this.parentElement.querySelectorAll('span').length, 10);
 	            var rem = parseInt(this.classList[0], 10);
 			    var diff = num - (rem - 1);
-			    var numOfPileOne = document.getElementById("r0").querySelectorAll('span').length;
-			    var numOfPileTwo = document.getElementById("r1").querySelectorAll('span').length;
 	            
                 if(move[pile]<=1 || rem-1<1 || diff<1){
                     console.error('Nincs elég/nem marad elég korong a sorban, hogy kettéoszd.');
@@ -119,7 +71,6 @@ var gameBoard = function(nim) {
                 }
 	        }
     	}	
-
     }
 
 
@@ -145,14 +96,10 @@ var gameBoard = function(nim) {
         //loop through the board
         for (var i in board) {
             if (board.hasOwnProperty(i) && typeof i !== 'undefined') {
-                //get images
-				var frag;
-			    //var serverPrefix = '';
-                
-                frag = drawPile(board[i]);
+				var frag = drawPile(board[i]);
                 //append images to the pile
-                emptyPile(container.querySelector('#r' + i));
-			    container.querySelector('#r' + i).appendChild(frag);
+                emptyPile(container.querySelector('#row_' + i));
+			    container.querySelector('#row_' + i).appendChild(frag);
             }
         }
         appendEventsToBoard();
@@ -177,26 +124,21 @@ var game = (function() {
     });
 
     var checkGame = function() {
-        document.querySelector('.whos').innerHTML = n.status().player;
-        if(document.querySelector('.whos').innerHTML === "Mi jövünk.")document.querySelector('.move').innerHTML = '';
-        else document.querySelector('.move').innerHTML = 'Kattints egy korongra, hogy azzal kettéosztd azt a kupacot. Amelyik korongra kattintasz, az és a tőle jobbra lévők kerülnek az új kupacba.';
+        document.querySelector('.game__step-cta-text').innerHTML = n.status().player;
+        if(document.querySelector('.game__step-cta-text').innerHTML === "Mi jövünk.")document.querySelector('.game__step-description').innerHTML = '';
+        else document.querySelector('.game__step-description').innerHTML = 'Kattints egy korongra, hogy azzal kettéosztd azt a kupacot. Amelyik korongra kattintasz, az és a tőle jobbra lévők kerülnek az új kupacba.';
         
         if(!n.status().isGameOn) {
-            document.querySelector('.move').innerHTML = '';
+            document.querySelector('.game__step-description').innerHTML = '';
             
 			document.getElementById("startTrue").style.display = 'none';
 			document.getElementById("startFalse").style.display = 'none';
 
 			document.getElementById("resetGame").style.display = 'block';
-			//var time = Math.floor(Math.random() * 1500 + 1500);
-        	//setTimeout(reset, time);
-            //document.querySelector('.repeat').style.display = '';
-
         }
     }
 
     var aiMove = function() {
-        //var aiMove =  n.board();//ai.makeMove(n.board(), n.isBeginningOfGame);
         if(n.status().isGameOn){
         	board.drawBoard(n.move(ai.makeMove(n.board())));
 	        checkGame();
@@ -205,18 +147,14 @@ var game = (function() {
 
     var startGameAsPlayer = function(player) {
         n.startGameAsPlayer(player);
-        //document.querySelector('.whos').innerHTML = 'Te jössz.';
-    
-        //document.querySelector('.repeat').style.display = 'none';
-
 
 		document.getElementById("startTrue").style.display = 'none';
 		document.getElementById("startFalse").style.display = 'none';
 
 		document.getElementById("resetGame").style.display = 'block';
          
-        document.querySelector('.game').style.display = 'block';
-        document.querySelector('.whos').innerHTML = n.status().player;
+        document.querySelector('.game__game-area').style.display = 'block';
+        document.querySelector('.game__step-cta-text').innerHTML = n.status().player;
     	board.drawBoard(n.board());
         checkGame();
         if(player){
@@ -226,8 +164,8 @@ var game = (function() {
 
     var resetGame = function() {
         board.drawBoard(n.newBoard());
-        document.querySelector('.move').innerHTML = '';
-        document.querySelector('.whos').innerHTML = 'A gombra kattintva tudod elindítani a játékot.';
+        document.querySelector('.game__step-description').innerHTML = '';
+        document.querySelector('.game__step-cta-text').innerHTML = 'A gombra kattintva tudod elindítani a játékot.';
     	document.getElementById("startTrue").style.display = 'block';
 		document.getElementById("startFalse").style.display = 'block';
 		document.getElementById("resetGame").style.display = 'none';
