21ba96fc3f82d57bf311b9d7268333a95fe9f765
Ildiko Czeller <czeildi@gmail.com>
2022-05-15 16:11:57 +0200

    update html loader
    Fixes #17

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,10 @@ module.exports = {
       rules: [
         {
           test: /\.html$/,
-          loader: 'html-loader'
+          loader: 'html-loader',
+          options: {
+            esModule: false
+          }
         }
       ]
     }
