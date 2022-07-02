baba2e2b30d6709c2aeed1d2994c80725d0b1616
Ildiko Czeller <czeildi@gmail.com>
2022-07-02 19:52:19 +0200

    add ability to kill group with click on soldier
    Closes #48

--- src/components/games/hunyadi-and-the-janissaries/hunyadi-and-the-janissaries.js
+++ src/components/games/hunyadi-and-the-janissaries/hunyadi-and-the-janissaries.js
@@ -6,6 +6,9 @@ import { getGameStateAfterKillingGroup } from './strategy/strategy';
 export default {
   template: require('./hunyadi-and-the-janissaries.html'),
   components: { EnemyLoader, SoldierSvg },
+  data: () => ({
+    hoveredPiece: null
+  }),
   computed: {
     ...mapState({ isPlayerSultan: (state) => state.isPlayerTheFirstToMove }),
     ...mapState(['board', 'shouldPlayerMoveNext']),
@@ -17,11 +20,31 @@ export default {
     ]),
     stepDescription() {
       if (!this.shouldPlayerMoveNext) return '';
-      return this.isPlayerSultan ? 'Kattints a katonákra és válaszd két részre a seregedet.' : '';
+      return this.isPlayerSultan
+        ? 'Kattints a katonákra és válaszd két részre a seregedet.'
+        : 'Kattints egy katonára vagy az alábbi gombok valamelyikére, hogy megsemmisítsd a vele azonos színű sereget.';
+    },
+    groupOfHoveredPiece() {
+      if (!this.hoveredPiece) return null;
+      return this.board[this.hoveredPiece.rowIndex][this.hoveredPiece.pieceIndex];
     }
   },
   methods: {
     ...mapActions(['endPlayerTurn', 'startGameWithRoleSelection', 'initializeGame']),
+    clickOnSoldier(rowIndex, pieceIndex) {
+      if (!this.shouldPlayerMoveNext) return;
+      if (this.isPlayerSultan) {
+        this.toggleGroup(rowIndex, pieceIndex);
+        return;
+      } else {
+        this.killGroup(this.board[rowIndex][pieceIndex]);
+      }
+    },
+    showToBeKilled(group) {
+      if (!this.shouldPlayerMoveNext || this.isPlayerSultan) return false;
+      if (!this.hoveredPiece) return false;
+      return group === this.groupOfHoveredPiece;
+    },
     toggleGroup(rowIndex, pieceIndex) {
       if (!this.shouldPlayerMoveNext || !this.isPlayerSultan) return;
       this.board[rowIndex][pieceIndex] = this.board[rowIndex][pieceIndex] === 'blue' ? 'red' : 'blue';
@@ -31,6 +54,13 @@ export default {
     },
     killGroup(group) {
       this.endPlayerTurn(getGameStateAfterKillingGroup(this.board, group));
+    },
+    resetTurnState() {
+      this.hoveredPiece = null;
+    },
+    restartGame() {
+      this.resetTurnState();
+      this.initializeGame();
     }
   },
   created() {
