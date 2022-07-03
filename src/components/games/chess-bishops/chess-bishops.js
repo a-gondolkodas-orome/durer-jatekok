4b9257f57db1489b3b246daa5eea8af4d270432f
Ildiko Czeller <czeildi@gmail.com>
2022-07-03 10:37:11 +0200

    improve next move feedback in chess bishops game
    #46

--- src/components/games/chess-bishops/chess-bishops.js
+++ src/components/games/chess-bishops/chess-bishops.js
@@ -1,3 +1,4 @@
+import { isEqual } from 'lodash-es';
 import { mapActions, mapState } from 'vuex';
 import GameSidebar from '../../common/game-sidebar/game-sidebar';
 import ChessBishopSvg from './chess-bishop-svg/chess-bishop-svg';
@@ -6,6 +7,9 @@ import { getGameStateAfterMove, getAllowedMoves } from './strategy/strategy';
 export default {
   template: require('./chess-bishops.html'),
   components: { GameSidebar, ChessBishopSvg },
+  data: () => ({
+    hoveredField: null
+  }),
   computed: {
     ...mapState(['board', 'shouldPlayerMoveNext']),
     stepDescription() {
@@ -15,11 +19,26 @@ export default {
   methods: {
     ...mapActions(['endPlayerTurn', 'initializeGame']),
     clickField({ row, col }) {
-      if (!this.shouldPlayerMoveNext) return;
-
-      if (!getAllowedMoves(this.board).map(({ row, col }) => row * 8 + col).includes(row * 8 + col)) return;
+      if (!this.isMoveAllowed({ row, col })) return;
 
       this.endPlayerTurn(getGameStateAfterMove(this.board, { row, col }));
+    },
+    isPotentialNextStep(field) {
+      if (!this.isMoveAllowed(field)) return false;
+      if (!this.hoveredField) return false;
+      return isEqual(this.hoveredField, field);
+    },
+    isMoveAllowed({ row, col }) {
+      if (!this.shouldPlayerMoveNext) return false;
+      return getAllowedMoves(this.board).map(({ row, col }) => row * 8 + col).includes(row * 8 + col);
+    },
+    wouldBeForbidden({ row, col }) {
+      if (!this.shouldPlayerMoveNext) return false;
+      if (!this.hoveredField) return false;
+      if (!this.isMoveAllowed(this.hoveredField)) return false;
+      if (!this.isMoveAllowed({ row, col })) return false;
+      if (this.isPotentialNextStep({ row, col })) return false;
+      return Math.abs(row - this.hoveredField.row) === Math.abs(col - this.hoveredField.col);
     }
   },
   created() {
