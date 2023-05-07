95b3a2e8837e3bdec913451b2e09157c772633c6
Ildiko Czeller <czeildi@gmail.com>
2023-05-07 22:26:03 +0200

    small refact in heap splitter 4

--- src/components/games/heap-splitting-games/heap-splitter-4/strategy/ai-step.js
+++ src/components/games/heap-splitting-games/heap-splitter-4/strategy/ai-step.js
@@ -1,55 +1,30 @@
 'use strict';
 
-import { random } from 'lodash-es';
+import { random, range, sample } from 'lodash-es';
 
 export const getBoardAfterAiStep = (board) => {
   const start = random(0, 3);
 
-  let odds = 0;
-
-  if (board[0] % 2 === 1) odds += 1;
-  if (board[1] % 2 === 1) odds += 1;
-  if (board[2] % 2 === 1) odds += 1;
-  if (board[3] % 2 === 1) odds += 1;
+  const odds = board.filter(p => p % 2 === 1).length;
 
   if (odds === 4) {
-    let first = start;
-    while (board[first] === 1) {
-      first += 1;
-      first %= 4;
-    }
+    const notSinglePileIndices = range(0, 3).filter(i => board[i] !== 1);
+    const first = sample(notSinglePileIndices);
     return getOptimalDivision(board, first, (first + 1) % 4);
   }
 
   if (odds === 3) {
-    let first = start;
-    while (board[first] % 2 === 1) {
-      first += 1;
-      first %= 4;
-    }
-    return getOptimalDivision(board, first, (first + 1) % 4);
+    const evenPileIndex = range(0, 3).find(i => board[i] % 2 === 0);
+    return getOptimalDivision(board, evenPileIndex, (evenPileIndex + 1) % 4);
   }
 
   if (odds === 2) {
-    let first = start;
-    while (board[first] % 2 === 1) {
-      first += 1;
-      first %= 4;
-    }
-    let second = (first + 1) % 4;
-    while (board[second] % 2 === 1) {
-      second += 1;
-      second %= 4;
-    }
-    return getOptimalDivision(board, first, second);
+    const evenPileIndices = range(0, 3).filter(i => board[i] % 2 === 0);
+    return getOptimalDivision(board, evenPileIndices[0], evenPileIndices[1]);
   }
 
   if (odds === 1) {
-    let oddPile = 0;
-    while (board[oddPile] % 2 === 0) {
-      oddPile += 1;
-      oddPile %= 4;
-    }
+    const oddPile = range(0, 3).find(i => board[i] % 2 === 1);
     if (
       board[oddPile] === 1 && board[(oddPile + 1) % 4] === 2 &&
       board[(oddPile + 2) % 4] === 2 && board[(oddPile + 3) % 4] === 2
