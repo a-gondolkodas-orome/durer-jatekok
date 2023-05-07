728672c4f04dde32eff27158e07e8496d10b0b4c
Ildiko Czeller <czeildi@gmail.com>
2023-05-07 23:15:19 +0200

    try to win chess bishops

--- src/components/games/chess-bishops/strategy/strategy.js
+++ src/components/games/chess-bishops/strategy/strategy.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { flatMap, range, sample } from 'lodash-es';
+import { flatMap, range, sample, cloneDeep } from 'lodash-es';
 
 export const generateNewBoard = () => Array(64).fill(null);
 
@@ -23,9 +23,26 @@ const getOptimalAiMove = (board) => {
   const allowedMirrorMoves = flatMap(range(0, 8), row => range(0, 8).map(col => ({ row, col })))
     .filter(({ row, col }) => board[row * 8 + col] === null && board[row * 8 + 7 - col] === 'bishop');
 
-  if (allowedMirrorMoves.length > 0) return sample(allowedMirrorMoves);
+  // we are playing according to optimal winning strategy
+  if (allowedMirrorMoves.length === 1) return allowedMirrorMoves[0];
 
   const allowedMoves = getAllowedMoves(board);
+  // make a random move if player has played optimally so far
+  if (allowedMirrorMoves.length === 0) return sample(allowedMoves);
+
+  // try to win from bad position if player does not play optimally
+  // following optimal strategy at the second step seems to slow
+  if (board.filter(b => b === 'bishop').length >= 4) {
+    const optimalPlaces = allowedMoves.filter(({ row, col }) => {
+      const boardCopy = cloneDeep(board);
+      markForbiddenFields(boardCopy, { row, col });
+      boardCopy[row * 8 + col] = 'bishop';
+      return isWinningState(boardCopy, false);
+    });
+
+    if (optimalPlaces.length > 0) return sample(optimalPlaces);
+  }
+
   return sample(allowedMoves);
 };
 
@@ -49,4 +66,21 @@ const markForbiddenFields = (board, { row, col }) => {
       board[(row - i) * 8 + col + i] = 'forbidden';
     }
   });
+};
+
+// given board *after* your step, are you set up to win the game for sure?
+const isWinningState = (board, amIPlayer) => {
+  if (getAllowedMoves(board).length === 0) {
+    return true;
+  }
+
+  const allowedPlacesForOther = getAllowedMoves(board, !amIPlayer);
+
+  const optimalPlaceForOther = allowedPlacesForOther.find(({ row, col }) => {
+    const boardCopy = cloneDeep(board);
+    markForbiddenFields(boardCopy, { row, col });
+    boardCopy[row * 8 + col] = 'bishop';
+    return isWinningState(boardCopy, !amIPlayer);
+  });
+  return optimalPlaceForOther === undefined;
 };
