c1cea0685f4024f86fb1829bc843fd52430a3cb1
Ildiko Czeller <czeildi@gmail.com>
2023-05-16 20:00:51 +0200

    csukott-nyitható szabályleírás
    Fixes #28

--- src/components/common/game-rule/game-rule.js
+++ src/components/common/game-rule/game-rule.js
@@ -1,4 +1,9 @@
+import { Disclosure,  DisclosureButton, DisclosurePanel } from '@headlessui/vue';
+
 export default {
   name: 'game-rule',
-  template: require('./game-rule.html')
+  template: require('./game-rule.html'),
+  components: {
+    Disclosure, DisclosureButton, DisclosurePanel
+  }
 };
