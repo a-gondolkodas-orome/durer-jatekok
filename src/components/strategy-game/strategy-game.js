63fa287533acbd80f4d5324d4274a5fd2f4a87ed
Ildiko Czeller <czeildi@gmail.com>
2023-05-17 20:41:33 +0200

    modal a játék végi nyertél / nem nyertél üzenetnek
    Closes #29

--- src/components/strategy-game/strategy-game.js
+++ src/components/strategy-game/strategy-game.js
@@ -1,26 +1,48 @@
-import { mapMutations, mapState } from 'vuex';
+import { mapMutations, mapState, mapGetters, mapActions } from 'vuex';
 import { gameComponents } from '../games/games';
 import PageNotFound from '../page-not-found/page-not-found';
+import { Dialog, DialogPanel, DialogDescription, DialogTitle } from '@headlessui/vue';
 
 export default {
   template: require('./strategy-game.html'),
   components: {
     ...gameComponents,
-    PageNotFound
+    PageNotFound,
+    Dialog,
+    DialogPanel,
+    DialogDescription,
+    DialogTitle
   },
   props: {
     gameId: String
   },
+  data: () => ({
+    isGameEndDialogOpen: false
+  }),
   computed: {
-    ...mapState(['gameDefinition'])
+    ...mapState(['gameDefinition', 'isPlayerWinner']),
+    ...mapGetters(['isGameFinished']),
+    gameEndText() {
+      return this.isPlayerWinner
+        ? 'Nyertél. Gratulálunk! :)'
+        : 'Sajnos, most nem nyertél, de ne add fel.';
+    }
   },
   methods: {
-    ...mapMutations(['setGameDefinition', 'setGameStatus']),
+    ...mapMutations(['setGameDefinition']),
+    ...mapActions(['initializeGame']),
     goBackToOverview() {
       this.$router.push('/');
     },
     setGameBasedOnRoute() {
       this.setGameDefinition({ gameId: this.gameId });
+    },
+    setIsGameEndDialogOpen(newValue) {
+      this.isGameEndDialogOpen = newValue;
+    },
+    startNewGame() {
+      this.isGameEndDialogOpen = false;
+      this.initializeGame();
     }
   },
   mounted() {
@@ -29,6 +51,9 @@ export default {
   watch: {
     gameId() {
       this.setGameBasedOnRoute();
+    },
+    isGameFinished(newValue) {
+      this.isGameEndDialogOpen = newValue;
     }
   }
 };
