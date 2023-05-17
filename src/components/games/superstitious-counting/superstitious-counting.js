719800cb89b3b088f6b88d3836a929d5ece25058
Ildiko Czeller <czeildi@gmail.com>
2023-05-17 19:50:16 +0200

    babonás számolós intuitívabb kinézet
    Closes #68

--- src/components/games/superstitious-counting/superstitious-counting.js
+++ src/components/games/superstitious-counting/superstitious-counting.js
@@ -2,26 +2,29 @@ import { mapActions, mapState } from 'vuex';
 import GameSidebar from '../../common/game-sidebar/game-sidebar';
 import GameRule from '../../common/game-rule/game-rule';
 import { getGameStateAfterMove } from './strategy/strategy';
+import { range } from 'lodash-es';
 
 export default {
   template: require('./superstitious-counting.html'),
   components: { GameSidebar, GameRule },
-  data: () => ({ step: 1 }),
   computed: {
-    ...mapState(['board', 'shouldPlayerMoveNext'])
+    ...mapState(['board', 'shouldPlayerMoveNext']),
+    fields() {
+      return range(this.board.target + 1);
+    }
   },
   methods: {
     ...mapActions(['endPlayerTurn', 'initializeGame']),
-    attemptStep() {
-      if (!this.shouldPlayerMoveNext) return;
-      if(this.step === this.board.restricted || this.step <= 0 || this.step >= 13) {
-        alert("Ez a lépés nem megengedett");
-        return;
+    isMoveAllowed(step) {
+      if (!this.shouldPlayerMoveNext) return false;
+      if(step === this.board.restricted || step <= 0 || step >= 13) {
+        return false;
       }
-      this.endPlayerTurn(getGameStateAfterMove(this.board, this.step));
+      return true;
     },
-    resetTurnState() {
-      this.step = 1;
+    makeStep(step) {
+      if (!this.isMoveAllowed(step)) return;
+      this.endPlayerTurn(getGameStateAfterMove(this.board, step));
     }
   },
   created() {
