8468f10c84022f77fa31d991c7f0bc096f10e781
Ildiko Czeller <czeildi@gmail.com>
2023-05-23 21:59:56 +0200

    migrate more games to pinia

--- src/components/games/pile-splitting-games/pile-splitter-3/pile-splitter-3.spec.js
+++ src/components/games/pile-splitting-games/pile-splitter-3/pile-splitter-3.spec.js
@@ -1,39 +1,36 @@
 'use strict';
 
 import PileSplitter3 from './pile-splitter-3';
-import createStore from '../../../../store/store';
 import { cloneDeep } from 'lodash-es';
 import { flushPromises } from '@vue/test-utils';
-import { mountComponentVuex } from '../../../../../test-helpers';
-
-const mountPileSplitter3 = async () => {
-  const store = createStore();
-  store.commit('setGameDefinition', { gameId: 'PileSplitter3' });
-  return await mountComponentVuex(PileSplitter3, { store });
-};
+import { mountComponent } from '../../../../../test-helpers';
+import { useGameStore } from '../../../../stores/game';
 
 describe('PileSplitter3', () => {
   it('should set selected role for player and start game accordingly', async () => {
-    const { store, wrapper } = await mountPileSplitter3();
+    const { wrapper } = await mountComponent(PileSplitter3);
+    const store = useGameStore();
 
     wrapper.find('.js-first-player').trigger('click');
 
-    expect(store.state.isPlayerTheFirstToMove).toBe(true);
-    expect(store.state.gameStatus).toEqual('inProgress');
+    expect(store.isPlayerTheFirstToMove).toBe(true);
+    expect(store.gameStatus).toEqual('inProgress');
   });
 
   it('should start a new game when button is pressed', async () => {
-    const { store, wrapper } = await mountPileSplitter3();
+    const { wrapper } = await mountComponent(PileSplitter3);
+    const store = useGameStore();
     wrapper.find('.js-first-player').trigger('click');
 
     wrapper.find('.js-restart-game').trigger('click');
 
-    expect(store.state.gameStatus).toEqual('readyToStart');
+    expect(store.gameStatus).toEqual('readyToStart');
   });
 
   it('should apply player move correctly', async () => {
-    const { store, wrapper } = await mountPileSplitter3();
-    const initialBoard = cloneDeep(store.state.board);
+    const { wrapper } = await mountComponent(PileSplitter3);
+    const store = useGameStore();
+    const initialBoard = cloneDeep(store.board);
 
     wrapper.find('.js-first-player').trigger('click');
     await flushPromises();
@@ -41,7 +38,7 @@ describe('PileSplitter3', () => {
     wrapper.findAll('.js-pile')[1].findAll('.js-pebble')[1].trigger('click');
     wrapper.findAll('.js-pile')[0].findAll('.js-pebble')[1].trigger('click');
 
-    expect(store.state.board).toEqual([
+    expect(store.board).toEqual([
       2,
       initialBoard[0] - 2,
       initialBoard[2]
@@ -49,8 +46,9 @@ describe('PileSplitter3', () => {
   });
 
   it('should not allow player move while enemy move is in progress', async () => {
-    const { store, wrapper } = await mountPileSplitter3();
-    const initialBoard = cloneDeep(store.state.board);
+    const { wrapper } = await mountComponent(PileSplitter3);
+    const store = useGameStore();
+    const initialBoard = cloneDeep(store.board);
 
     wrapper.find('.js-second-player').trigger('click');
     await flushPromises();
@@ -58,6 +56,6 @@ describe('PileSplitter3', () => {
     wrapper.findAll('.js-pile')[1].findAll('.js-pebble')[1].trigger('click');
     wrapper.findAll('.js-pile')[0].findAll('.js-pebble')[1].trigger('click');
 
-    expect(store.state.board).toEqual(initialBoard);
+    expect(store.board).toEqual(initialBoard);
   });
 });
