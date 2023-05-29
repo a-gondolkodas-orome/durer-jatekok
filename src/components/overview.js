b5c1a180734c7a3053ab1c455581219527e63bf7
Ildiko Czeller <czeildi@gmail.com>
2023-05-29 22:39:43 +0200

    add year filter to overview

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,22 +1,30 @@
 import { gameList } from './games/gameList';
 import { Listbox } from '@headlessui/react';
 import { useState } from 'react';
+import { uniq } from 'lodash-es';
 
 export const Overview = () => {
   const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedYears, setSelectedYears] = useState([]);
 
   const shouldShow = game => {
     if (game.isHiddenFromOverview) return false;
-    return selectedCategories.includes(game.category) || selectedCategories.length === 0;
+    if (selectedCategories.length > 0 && !selectedCategories.includes(game.category)) return false;
+    if (selectedYears.length > 0 && !selectedYears.includes(game.year)) return false;
+    return true;
   };
 
   return <main className="p-2">
     <OverviewHeader></OverviewHeader>
-    <div class="flex flex-wrap align-baseline">
+    <div className="flex flex-wrap align-baseline">
       <CategoryFilter
         selectedCategories={selectedCategories}
         setSelectedCategories={setSelectedCategories}
       ></CategoryFilter>
+      <YearFilter
+        selectedYears={selectedYears}
+        setSelectedYears={setSelectedYears}
+      ></YearFilter>
     </div>
     <div className="flex flex-wrap">
       {Object.keys(gameList).filter(id => shouldShow(gameList[id])).map(id => Game(id, gameList[id]))}
@@ -30,10 +38,13 @@ const CategoryFilter = ({ selectedCategories, setSelectedCategories }) => {
   return <Listbox
     value={selectedCategories} onChange={setSelectedCategories}
     as="div" multiple horizontal
-    class="mb-2 w-96 inline-block"
+    className="mb-2 w-96 inline-block px-1"
   >
-    <label for="category-selector" className="block">Kategória szűrő:</label>
-    <Listbox.Button id="category-selector" className="border-2 border-slate-600 rounded w-full">
+    <label htmlFor="category-selector" className="block">Kategória szűrő:</label>
+    <Listbox.Button
+      id="category-selector"
+      className="border-2 border-slate-600 rounded w-full"
+    >
       {selectedCategories.sort().join(', ') || 'Válassz kategóriákat'}
     </Listbox.Button>
     <Listbox.Options className="text-center mt-1">
@@ -44,6 +55,33 @@ const CategoryFilter = ({ selectedCategories, setSelectedCategories }) => {
       >{({ active, selected }) =>
         <span className={`border-2 rounded px-1 mx-1 inline-block ${selected ? 'bg-blue-200' : ''} ${active ? 'outline' : ''}`}>
           <span className={selected ? '' : 'text-transparent'}>✓</span>{category}
+        </span>
+      }</Listbox.Option>)}
+    </Listbox.Options>
+  </Listbox>;
+};
+
+const YearFilter = ({ selectedYears, setSelectedYears }) => {
+  const allGames = Object.values(gameList).filter(game => !game.isHiddenFromOverview);
+  const allYears = uniq(allGames.map(game => game.year)).sort((a, b) => Number(a) - Number(b));
+
+  return <Listbox
+    value={selectedYears} onChange={setSelectedYears}
+    as="div" multiple horizontal
+    className="mb-2 w-96 inline-block px-1"
+  >
+    <label htmlFor="year-selector" className="block">Kategória szűrő:</label>
+    <Listbox.Button id="year-selector" className="border-2 border-slate-600 rounded w-full">
+      {selectedYears.sort((a, b) => Number(a) - Number(b)).join(', ') || 'Válassz éveket'}
+    </Listbox.Button>
+    <Listbox.Options className="text-center mt-1">
+      {allYears.map(year => <Listbox.Option
+        key={year}
+        value={year}
+        className="inline-block"
+      >{({ active, selected }) =>
+        <span className={`border-2 rounded px-1 mx-1 inline-block ${selected ? 'bg-blue-200' : ''} ${active ? 'outline' : ''}`}>
+          <span className={selected ? '' : 'text-transparent'}>✓</span>{year}
         </span>
       }</Listbox.Option>)}
     </Listbox.Options>
