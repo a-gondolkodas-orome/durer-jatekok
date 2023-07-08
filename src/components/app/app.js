28cde6c54777cdc95dfa34e628b04810763e4fa1
Ildiko Czeller <czeildi@gmail.com>
2023-07-08 17:09:34 +0200

    update some packages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,12 @@ import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { Overview } from '../overview/overview';
 import { ErrorPage } from '../error-page';
 import { Demonstration } from '../games/demonstration/demonstration';
+import { TwoTimesTwo } from '../games/two-times-two/two-times-two';
 
 export const App = () => {
   const router = createHashRouter([
     { path: '/', element: <Overview />, errorElement: <ErrorPage /> },
+    { path: '/game/TwoTimesTwo', element: <TwoTimesTwo /> },
     { path: '/game/:gameId', element: <Demonstration /> }
   ]);
   return <StrictMode>
