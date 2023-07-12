0133dcbdc96ccdf3339ee7b0ec243205ae5db471
Ildiko Czeller <czeildi@gmail.com>
2023-07-12 23:07:48 +0200

    allow connecting parallel nodes

--- src/components/games/triangular-grid-ropes/triangular-grid-ropes.js
+++ src/components/games/triangular-grid-ropes/triangular-grid-ropes.js
@@ -1,25 +1,77 @@
 import React, { useState } from 'react';
 import { strategyGameFactory } from '../strategy-game';
+import { isEqual, range } from 'lodash';
 
-const GameBoard = ({ board, ctx }) => {
+//    0
+//   1 2
+//  3 4 5
+// 6 7 8 9
+
+const vertices = [
+  { id: 0, x: 0, y: 3, z: 3, cx: "50%", cy: "12.5%" },
+  { id: 1, x: 1, y: 2, z: 3, cx: "41.625%", cy: "27.5%" },
+  { id: 2, x: 1, y: 3, z: 2, cx: "58.375%", cy: "27.5%" },
+  { id: 3, x: 2, y: 1, z: 3, cx: "33.25%", cy: "42.5%" },
+  { id: 4, x: 2, y: 2, z: 2, cx: "50%", cy: "42.5%" },
+  { id: 5, x: 2, y: 3, z: 1, cx: "66.75%", cy: "42.5%" },
+  { id: 6, x: 3, y: 0, z: 3, cx: "25%", cy: "57.5%" },
+  { id: 7, x: 3, y: 1, z: 2, cx: "41.625%", cy: "57.5%" },
+  { id: 8, x: 3, y: 2, z: 1, cx: "58.375%", cy: "57.5%" },
+  { id: 9, x: 3, y: 3, z: 0, cx: "75%", cy: "57.5%" }
+];
+
+const isParallel = (nodeA, nodeB) => {
+  const vertexA = vertices[nodeA];
+  const vertexB = vertices[nodeB];
+  return vertexA.x === vertexB.x || vertexA.y === vertexB.y || vertexA.y === vertexB.y;
+};
+
+const GameBoard = ({ board, setBoard, ctx }) => {
+  const [firstNode, setFirstNode] = useState(null);
+
+  const isConnected = (nodeA, nodeB) => {
+    return board.some(edge =>
+      isEqual(edge, ({ from: nodeA, to: nodeB })) || isEqual(edge, ({ from: nodeB, to: nodeA }))
+    );
+  };
+
+  const connectNode = id => {
+    if (!ctx.shouldPlayerMoveNext) return;
+    if (firstNode === null) {
+      setFirstNode(id);
+    } else if (id === firstNode) {
+      setFirstNode(null);
+    } else {
+      if (!isParallel(firstNode, id)) return;
+      const newBoard = [...board];
+      newBoard.push({ from: firstNode, to: id });
+      setBoard(newBoard);
+      setFirstNode(null);
+    }
+  };
   return (
   <section className="p-2 shrink-0 grow basis-2/3">
-
   <svg className="aspect-square">
-    {/* <!-- row 1 --> */}
-    <circle cx="50%" cy="12.5%" r="1%" fill="black"/>
-    {/* <!-- row 2 --> */}
-    <circle cx="41.625%" cy="27.5%" r="1%" fill="black"/>
-    <circle cx="58.375%" cy="27.5%" r="1%" fill="black"/>
-    {/* <!-- row 3 -->/ */}
-    <circle cx="33.25%" cy="42.5%" r="1%" fill="black"/>
-    <circle cx="50%" cy="42.5%" r="1%" fill="black"/>
-    <circle cx="66.75%" cy="42.5%" r="1%" fill="black"/>
-    {/* <!-- row 4 --> */}
-    <circle cx="25%" cy="57.5%" r="1%" fill="black"/>
-    <circle cx="41.625%" cy="57.5%" r="1%" fill="black"/>
-    <circle cx="58.375%" cy="57.5%" r="1%" fill="black"/>
-    <circle cx="75%" cy="57.5%" r="1%" fill="black"/>
+    {vertices.map(vertex => (
+      <circle
+        key={vertex.id}
+        cx={vertex.cx} cy={vertex.cy} r="2%" fill="black"
+        className={`${vertex.id === firstNode ? 'fill-slate-600' : ''}`}
+        onClick={() => connectNode(vertex.id)}
+      />
+    ))}
+
+    {range(10).map(from => <>
+      {range(10).map(to => <>
+        {isConnected(from, to) && (
+          <line
+            x1={vertices[from].cx} y1={vertices[from].cy}
+            x2={vertices[to].cx} y2={vertices[to].cy}
+            stroke="black" strokeWidth="2"
+          />
+        )}
+      </>)}
+    </>)}
   </svg>
   </section>
   );
