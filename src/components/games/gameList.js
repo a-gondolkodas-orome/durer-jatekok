24dfd2e5b8125d237bec009763768501699e51a9
Ildiko Czeller <czeildi@gmail.com>
2023-12-19 14:03:33 +0100

    temporarily hide not working game
    Related issue: #99

--- src/components/games/gameList.js
+++ src/components/games/gameList.js
@@ -154,11 +154,5 @@ export const gameList = {
     round: 'döntő',
     category: ['B'],
     name: '10 korong'
-  },
-  PrimeExponentials: {
-    year: 1,
-    round: 'döntő',
-    category: ['D'],
-    name: 'Prímhatványok kivonása'
   }
 };
