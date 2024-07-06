d9ebad117d20cd29523e86bfd9601d70aabe6efe
Ildikó Czeller <czeildi@users.noreply.github.com>
2024-07-06 12:29:11 +0200

    Bacteria: ux and refactor improvements (#107)
    * clarify target, empty, goal cells
    
    * refact new board generation
    
    * format document
    
    * wip show possible moves and refactor clickField
    
    * better show multiple bacteria in single cell
    
    * small refact
    
    * fix winner
    
    * ai should always move, at least randomly

--- src/components/games/bacteria/strategy/strategy.js
+++ src/components/games/bacteria/strategy/strategy.js
@@ -12,23 +12,6 @@ export const getGameStateAfterAiTurn = ({ board, playerIndex }) => {
   }
 };
 
-const compareCellsLeft = ({first, second}) => {
-    const firstAxis = Math.floor(0.5*first[0]+first[1]);
-    const secondAxis = Math.floor(0.5*second[0]+second[1]);
-
-    if (first === second) return 0;
-    else {
-        if (firstAxis > secondAxis || (firstAxis === secondAxis && first[0] < second[0])) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }
-}
-
-let defensePaths = [];
-let attackCells = [];
-
 const whoWins = (board) => {
     if (getSizeOfBoard(board) + 2*4 - getGoals(board).length < getBacteria(board).length) {
         return 0;
@@ -53,51 +36,15 @@ const whoWins = (board) => {
         }
 
         const bacteria = getBacteria(board);
-        // in case we want ai to find winning state from a later position, this ordering is necessary
-        /*bacteria.sort((a,b) => {a === b ? 0 : (Math.floor(0.5*a[0]+a[1]) > Math.floor(0.5*b[0]+b[1]) ||
-            (Math.floor(0.5*a[0]+a[1]) === Math.floor(0.5*b[0]+b[1]) && a[0] < b[0]) ? 1 : -1)});*/
         let deficient = false;
         let reached = false;
-        //let jStart = -1;
         for(let j = 0; j<bacteria.length && !deficient; j++) {
-            let reachables = reachable({row: bacteria[j][0], col: bacteria[j][1],board: freeBoard});
             reached = false;
-            /*let newPath = [];
-            if (reachables.length > 0) {
-                let row = reachables[0][0]; let col = reachables[0][1];
-                freeBoard[row][col] = -3;
-                reached = true;
-                for(let pathRowIt = bacteria[j][0]; pathRowIt < row; pathRowIt++) {
-                    newPath.push([pathRowIt,col]);
-                    freeBoard[pathRowIt][col] = -3;
-                    if (freeBoard[pathRowIt+1][col-0.5-0.5*(-1)**(pathRowIt+1)] >= 0) {
-                        col = col-0.5-0.5*(-1)**pathRowIt;
-                    } else {
-                        col = col+0.5+0.5*(-1)**(pathRowIt+1);
-                    }
-                }
-                newPath.push([row,reachables[0][1]]);
-                defensePaths.push(newPath);
-                setDefenseBoard(freeBoard);
-            }*/
             if (!reached) {
                 deficient = true;
-                //jStart = j;
             }
         };
         if (deficient) {
-            /*let iEnd = jStart;
-            for (let i = jStart; i>=0 && reached; i--) {
-                let reachables = reachable({row: bacteria[i][0], col: bacteria[i][1],board: freeBoard});
-                reached = false;
-                if (reachables.length > 0) {
-                    let rowFree = reachables[reachables.length-1][0]; let colFree = reachables[reachables.length-1][1];
-                    freeBoard[rowFree][colFree] = -3;
-                    attackCells.push([row,col]);
-                    reached = true;
-                }
-                if (!reached) {iEnd = i;}
-            }*/
             return 0;
         } else {
             return 1;
@@ -115,14 +62,12 @@ const aiDefense = (board) => {
         const [randomRow, randomCol] = sample(getBacteria(board));
         newBoard[randomRow][randomCol] -= 1;
     } else {
-        /*for (let row = 0; row < 9; row++) {
-            for (let col = 0; col < getSizeOfBoard(board)-1       ; col++) {
-
-            }
-        }*/
+        // TODO: follow strategy
+        const [randomRow, randomCol] = sample(getBacteria(board));
+        newBoard[randomRow][randomCol] -= 1;
     }
     const isGameEnd = getOccupied(newBoard).length === 0;
-    return {newBoard, isGameEnd};
+    return { newBoard, isGameEnd };
 }
 
 const aiAttack = (board) => {
@@ -130,7 +75,8 @@ const aiAttack = (board) => {
     let isGameEnd = false;
     if (!aiWins) {aiWins = whoWins(board) === 0;}
 
-    if (!aiWins) {
+    // TODO: if aiWins, follow strategy instead of random step
+    if (!aiWins || aiWins) {
         const [randomRow, randomCol] = sample(getBacteria(board));
         let attackOptions = range(3);
         if (randomRow > 6) {attackOptions.pop()}
@@ -175,10 +121,6 @@ const aiAttack = (board) => {
     return {newBoard, isGameEnd};
 }
 
-const findPaths = (board) => {
-
-}
-
 const getBacteria = (board) => {
     let bacteria = []
     for (let row = 0; row < board.length; row++) {
@@ -209,32 +151,4 @@ const getGoals = (board) => {
 
 const getSizeOfBoard = (board) => {
     return board[0].length;
-}
-
-const reachable = ({row,col,board}) => {
-    const boardSize = getSizeOfBoard(board);
-    const cells = [];
-    let leftReachBorderRow = 8;
-    let leftReachBorderCol = Math.ceil(0.5*row-4+col);
-    if (leftReachBorderCol < 0) {
-        leftReachBorderRow = 2*Math.ceil(col-0.5*row);
-        leftReachBorderCol = 0;
-        for (let rowIt = leftReachBorderRow; rowIt < 9; rowIt += 2) {
-            if (board[rowIt][leftReachBorderCol] >= 0) { cells.push([rowIt,leftReachBorderCol])}
-        }
-    }
-    let rightReachBorderRow = 8;
-    let rightReachBorderCol = Math.floor(4-0.5*row+col);
-
-    for (let colIt = leftReachBorderCol; colIt <= Math.min(boardSize-1,rightReachBorderCol); colIt++) {
-        if (board[rightReachBorderRow][colIt] >= 0) {cells.push([rightReachBorderRow,colIt])}
-    }
-    if (rightReachBorderCol >= boardSize) {
-        rightReachBorderCol = boardSize-1;
-        rightReachBorderRow = 2*Math.floor(boardSize- col+0.5*row);
-        for (let rowIt = 6; rowIt >= rightReachBorderRow; rowIt -= 2) {
-            if (board[rowIt][rightReachBorderCol] >= 0) { cells.push([rowIt,rightReachBorderCol])}
-        }
-    }
-    return cells;
 }
