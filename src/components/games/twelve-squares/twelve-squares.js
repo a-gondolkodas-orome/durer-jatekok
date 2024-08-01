c08d92ceedb1c32382739d128730820a9d527d69
Ildiko Czeller <czeildi@gmail.com>
2024-08-01 16:49:35 +0200

    refact and small fix in twelve squares

--- src/components/games/twelve-squares/twelve-squares.js
+++ src/components/games/twelve-squares/twelve-squares.js
@@ -1,51 +1,62 @@
 import React, { useState } from 'react';
 import { strategyGameFactory } from '../strategy-game';
-import { range } from 'lodash';
-import { getOptimalAiStep } from './strategy';
+import { range, random } from 'lodash';
 import { ChessBishopSvg } from '../chess-bishops/chess-bishop-svg';
 
 const generateStartBoard = () => {
   return { left: 1, right: 12 };
 };
 
-const getGameStateAfterMove = (board, step, moverIndex) => {
-	let l = board.left, r = board.right;
-
-  if(moverIndex === 0) {
-    l += step;
-    if(l === r) l++;
-  } else {
-    r -= step;
-    if(l === r) r--;
-  }
-
-  const isGameEnd = l > r;
-
-  return {
-	  nextBoard: { left: l, right: r },
-	  isGameEnd,
-	  winnerIndex: isGameEnd ? moverIndex : null
-  };
+const getGameStateAfterMove = (nextBoard) => {
+	return { nextBoard, isGameEnd: nextBoard.right < nextBoard.left, winnerIndex: null };
 };
 
 const getGameStateAfterAiTurn = ({ board, playerIndex }) => {
   const step = getOptimalAiStep(board);
-  return getGameStateAfterMove(board, step, 1-playerIndex);
+  const nextBoard = playerIndex === 0
+    ? { left: board.left, right: board.right - step }
+    : { left: board.left + step, right: board.right };
+  return getGameStateAfterMove(nextBoard);
+};
+
+const getOptimalAiStep = ({ left, right }) => {
+  let dst = right-left;
+  if(dst === 1) return 2;
+  if(dst === 2) return 1;
+  if(dst % 3 === 2) return random(1,2);
+  return (dst+1) % 3;
 };
 
 const GameBoard = ({ board, ctx }) => {
   const isMoveAllowed = (step) => {
 	  if(!ctx.shouldPlayerMoveNext) return false;
-	  if(ctx.playerIndex === 0) {
-      return (step > 0 && step <= 2 && step+board.left !== board.right);
-    }
-
-	  return (step < 0 && step >= -2 && step+board.right !== board.left);
+    if (step === board.right - board.left) return false;
+    return step === 1 || step === 2;
   };
 
   const makeStep = (step) => {
-	  // if (!isMoveAllowed(step)) return;
-	  ctx.endPlayerTurn(getGameStateAfterMove(board, step, ctx.playerIndex));
+    if (!isMoveAllowed(step)) return;
+    const nextBoard = ctx.playerIndex === 0
+      ? { left: board.left + step, right: board.right }
+      : { left: board.left, right: board.right - step };
+	  ctx.endPlayerTurn(getGameStateAfterMove(nextBoard));
+  };
+
+  const potentialStep = i => {
+    return ctx.playerIndex === 0 ? i - board.left : board.right - i;
+  }
+
+  const cellBackground = (i) => {
+    if (isMoveAllowed(potentialStep(i))) {
+      return 'bg-green-200 hover:bg-green-400';
+    }
+    if (( i === board.left && ctx.playerIndex === 0) || (i === board.right && ctx.playerIndex === 1)) {
+      return 'bg-green-400';
+    }
+    if (( i === board.left && ctx.playerIndex === 1) || (i === board.right && ctx.playerIndex === 0)) {
+      return 'bg-purple-400';
+    }
+    return 'bg-slate-200';
   };
 
   return (
@@ -56,20 +67,16 @@ const GameBoard = ({ board, ctx }) => {
       <tr>
         {range(1,13).map(i =>
           <td
-          className={`
-            text-xl text-center p-0.5 font-bold aspect-square border-2 border-white
-            ${isMoveAllowed(ctx.playerIndex === 0 ? i-board.left : i-board.right) ?
-              'bg-green-200 hover:bg-green-400' :
-              (( i === board.left && ctx.playerIndex === 0) || (i === board.right && ctx.playerIndex === 1) ? 'bg-green-400' :
-              ( i === board.left || i === board.right ) ? 'bg-purple-400' :'bg-slate-200')
-            }
-          `}
+            key={i}
+            className={`
+              text-xl text-center p-0.5 font-bold aspect-square border-2 border-white
+              ${cellBackground(i)}
+            `}
           >
             <button
-              key={i}
               className="w-full"
-              disabled={!isMoveAllowed(ctx.playerIndex === 0 ? i-board.left : i-board.right)}
-              onClick={() => makeStep(ctx.playerIndex === 0 ? i-board.left : board.right-i)}
+              disabled={!isMoveAllowed(potentialStep(i))}
+              onClick={() => makeStep(potentialStep(i))}
             >{ i === board.left || i === board.right
               ? <span>
                 <svg className="inline-block w-full aspect-square">
