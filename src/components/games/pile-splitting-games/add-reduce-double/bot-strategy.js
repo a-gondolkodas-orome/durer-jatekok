e36e5c832ee63255a7f8f58363c59da1aa99885c
Ildiko Czeller <czeildi@gmail.com>
2025-01-08 21:12:07 +0100

    use moves api

--- src/components/games/pile-splitting-games/add-reduce-double/bot-strategy.js
+++ src/components/games/pile-splitting-games/add-reduce-double/bot-strategy.js
@@ -2,25 +2,21 @@
 
 import { random } from 'lodash';
 
-export const getOptimalAiMove = (board) => {
-  let pileId, pieceId;
+export const aiBotStrategy = ({ board, moves }) => {
+  let pileId, pieceCount;
   if (board[0]-board[1] === -1 || board[0]-board[1] === 0 || board[0]-board[1] === 1) {
-    const ran=random(0,1);
-    pileId=(board[ran]>1)
-      ? ran
-      : 1-ran;
-    pieceId=generateRandomEvenBetween(2, board[pileId])-1;
+    const ran = random(0,1);
+    pileId=(board[ran]>1) ? ran : (1 - ran);
+    pieceCount = generateRandomEvenBetween(2, board[pileId]);
 
   } else {
     let third;
-    pileId=(board[0]>board[1])
-      ? 0
-      : 1;
-    third=Math.floor((board[pileId]-board[1-pileId]+1)/3);
-    pieceId=2*third-1;
+    pileId = (board[0]>board[1]) ? 0 : 1;
+    third = Math.floor((board[pileId]-board[1-pileId]+1)/3);
+    pieceCount = 2 * third;
   }
 
-  return { pileId, pieceId };
+  moves.moveHalvedPieces(board, { pileId, pieceCount });
 };
 
 const generateRandomEvenBetween = (low, high) => {
