60eae70131b64392e82e3e5bbfe88ae8a17efd0a
Ildiko Czeller <czeildi@gmail.com>
2025-12-14 21:29:39 +0100

    implement ai strategy

--- src/components/games/stones-remove-one-not-twice-from-left/stones-remove-one-not-twice-from-left.js
+++ src/components/games/stones-remove-one-not-twice-from-left/stones-remove-one-not-twice-from-left.js
@@ -6,7 +6,7 @@ const BoardClient = ({ board, ctx, moves }) => {
   const isMoveAllowed = pileId => {
     if (!ctx.shouldRoleSelectorMoveNext) return false;
     if (board.piles[pileId] === 0) return false;
-    if (pileId === 1 && board.isLastMoveFromLeftByPlayer[ctx.currentPlayer]) return false;
+    if (pileId === 0 && board.leftRestriction[ctx.currentPlayer]) return false;
     return true;
   }
 
@@ -40,7 +40,7 @@ const moves = {
   removeStone: (board, { ctx, events }, pileId) => {
     const nextBoard = cloneDeep(board);
     nextBoard.piles[pileId] = board.piles[pileId] - 1;
-    nextBoard.isLastMoveFromLeftByPlayer[ctx.currentPlayer] = (pileId === 0);
+    nextBoard.leftRestriction[ctx.currentPlayer] = (pileId === 0);
     events.endTurn();
     if (isGameEnd(nextBoard, ctx)) {
       events.endGame();
@@ -53,21 +53,85 @@ const isGameEnd = (board, ctx) => {
   if (isEqual(board.piles, [0, 0])) {
     return true;
   }
-  if (board.piles[1] === 0 && board.isLastMoveFromLeftByPlayer[1 - ctx.currentPlayer]) {
+  if (board.piles[1] === 0 && board.leftRestriction[1 - ctx.currentPlayer]) {
     return true;
   }
   return false;
 }
 
 const aiBotStrategy = ({ board, ctx, moves }) => {
-  const randomMove = getPileOfRandomAllowedMove(board, ctx);
-  moves.removeStone(board, randomMove);
+  if (board.leftRestriction[ctx.currentPlayer]) {
+    moves.removeStone(board, 1);
+    return;
+  }
+  const optimalMove = getOptimalMove(board, ctx);
+  const botMove = optimalMove !== undefined
+    ? optimalMove
+    : getPileOfRandomAllowedMove(board, ctx);
+  moves.removeStone(board, botMove);
 };
+
+// return undefined if there is no winning move
+const getOptimalMove = (board, ctx) => {
+  const otherPlayer = 1 - ctx.currentPlayer;
+  const parity = [board.piles[0] % 2 === 0, board.piles[1] % 2 === 0]
+
+  if (parity[0] && parity[1]) {
+    if (!board.leftRestriction[otherPlayer]) {
+      return undefined;
+    } else if (board.leftRestriction[ctx.currentPlayer]) {
+      console.error("Unexpected internal state, please report.")
+      return undefined;
+    } else {
+      /*
+      If we take right, the other must take right, then we are in an even-even
+      position without any restriction which is a losing position. Check winning
+      move in next round if we take left (and the other must take right). If
+      there is a winning move next round it means taking from left now is also a
+      winning move. Otherwise we do not have a winning move.
+      */
+      const nextRestriction = [false, false];
+      nextRestriction[ctx.currentPlayer] = true;
+      nextRestriction[1 -ctx.currentPlayer] = false;
+      const nextBoard = {
+        piles: [board.piles[0] - 1, board.piles[1] - 1],
+        leftRestriction: nextRestriction
+      }
+      const optimalMoveInNextRound = getOptimalMove(nextBoard, ctx);
+      return optimalMoveInNextRound !== undefined ? 0 : undefined;
+    }
+  }
+  if (parity[0] && !parity[1]) {
+    return 1;
+  }
+  if (!parity[0] && !parity[1]) {
+    if (board.piles[0] > board.piles[1]) {
+      return 1;
+    } else {
+      return undefined;
+    }
+  }
+  if (!parity[0] && parity[1]) {
+    if (board.piles[0] <= (board.piles[0] + 1)) {
+      if (!board.leftRestriction[ctx.currentPlayer]) {
+        return 0;
+      } else {
+        console.error("Unexpected internal state, please report.")
+        return undefined;
+      }
+    } else {
+      return undefined;
+    }
+  }
+
+  // we should not reach this branch;
+  return undefined;
+}
 
 const getPileOfRandomAllowedMove = (board, ctx) => {
   if (board.piles[0] === 0) return 1;
   if (board.piles[1] === 0) return 0;
-  if (board.isLastMoveFromLeftByPlayer[ctx.currentPlayer]) return 1;
+  if (board.leftRestriction[ctx.currentPlayer]) return 1;
   return random(0, 1);
 }
 
@@ -94,7 +158,7 @@ const generateStartBoard = () => {
   ])
   return {
     piles,
-    isLastMoveFromLeftByPlayer: [false, false]
+    leftRestriction: [false, false]
   };
 }
 
