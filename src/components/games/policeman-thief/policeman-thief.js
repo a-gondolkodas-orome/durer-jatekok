35e54ab470c7e87add0ebfe74600edf852d78cdb
Ildikó Czeller <czeildi@users.noreply.github.com>
2026-01-24 12:38:22 +0100

    Add 9kb policeman thief (#199)
    * add game skeleton
    
    * fix missing thief position

--- src/components/games/policeman-thief/policeman-thief.js
+++ src/components/games/policeman-thief/policeman-thief.js
@@ -6,13 +6,33 @@ import { aiBotStrategy } from "./bot-strategy";
 import { BoardClient } from "./board-client";
 import { gameList } from "../gameList";
 
-const generateStartBoard = () => {
+const generateStartBoardA = () => {
   const policeStartPosition = random(0, 7);
   const immediatePoliceWinPositions = [policeStartPosition, ...neighbours[policeStartPosition]];
   const thiefStartPosition = sample(difference(range(0, 8), immediatePoliceWinPositions));
   return {
     turnCount: 0,
     policemen: [policeStartPosition, policeStartPosition],
+    thief: thiefStartPosition,
+    firstPolicemanMoved: false
+  };
+};
+
+const generateStartBoardB = () => {
+  const policeStartPosition = [random(0, 7), random(0, 7)];
+  const immediatePoliceWinPositions = [
+    ...policeStartPosition,
+    ...neighbours[policeStartPosition[0]],
+    ...neighbours[policeStartPosition[1]]
+  ];
+  const thiefStartPositionOptions = difference(range(0, 8), immediatePoliceWinPositions);
+  if (thiefStartPositionOptions.length === 0) {
+    return generateStartBoardB();
+  }
+  const thiefStartPosition = sample(thiefStartPositionOptions);
+  return {
+    turnCount: 0,
+    policemen: policeStartPosition,
     thief: thiefStartPosition,
     firstPolicemanMoved: false
   };
@@ -58,7 +78,7 @@ const hasFirstPlayerWon = (board) => {
   return board.turnCount < 4 && board.policemen.includes(board.thief);
 };
 
-const rule = (
+const ruleA = (
   <>
     Az ábrán egy kisváros úthálózata látható, ahol az útkereszteződéseket
     pöttyök jelölik. A játék kezdetén a szervezők az egyik útkereszteződésbe
@@ -72,8 +92,21 @@ const rule = (
   </>
 );
 
-export const Policemanthief = strategyGameFactory({
-  rule,
+const ruleB = (
+  <>
+    Az ábrán egy kisváros úthálózata látható, ahol az útkereszteződéseket pöttyök
+    jelölik. A játék kezdetén a szervezők valamely útkereszteződésekbe leteszik a tolvajt
+    ábrázoló (piros), valamint a két rendőrt ábrázoló (kék illetve zöld) korongokat; a két rendőr
+    esetleg ugyanarra a mezőre is kerülhet. Egy körben előbb a rendőrök (a kék majd a zöld) mennek át
+    egy-egy szomszédos útkereszteződésbe egy út mentén (szét is válhatnak), majd a
+    tolvaj is hasonlóan lép. Minden körben kötelező mindenkinek helyet változtatnia.
+    A rendőrök nyernek, ha a rabló bármikor azonos kereszteződésben van egy rend-
+    őrrel. A tolvaj nyer, ha a harmadik kör végéig nem kapták el.
+  </>
+)
+
+export const PolicemanthiefA = strategyGameFactory({
+  rule: ruleA,
   metadata: gameList.Policemanthief,
   roleLabels: ["Rendőrök", "Tolvaj"],
   BoardClient,
@@ -84,7 +117,24 @@ export const Policemanthief = strategyGameFactory({
       return "Kattints arra az útkereszteződésre, ahová a tolvajjal lépni szeretnél.";
     }
   },
-  generateStartBoard,
+  generateStartBoard: generateStartBoardA,
+  aiBotStrategy,
+  moves
+});
+
+export const PolicemanthiefB = strategyGameFactory({
+  rule: ruleB,
+  metadata: gameList.PolicemanthiefB,
+  roleLabels: ["Rendőrök", "Tolvaj"],
+  BoardClient,
+  getPlayerStepDescription: ({ board, ctx }) => {
+    if (ctx.chosenRoleIndex === 0) {
+      return `Kattints arra az útkereszteződésre, ahová a ${board.firstPolicemanMoved ? "zöld" : "kék"} rendőrrel lépni szeretnél.`;
+    } else {
+      return "Kattints arra az útkereszteződésre, ahová a tolvajjal lépni szeretnél.";
+    }
+  },
+  generateStartBoard: generateStartBoardB,
   aiBotStrategy,
   moves
 });
