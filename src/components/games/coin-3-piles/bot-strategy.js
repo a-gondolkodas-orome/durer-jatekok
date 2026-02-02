02934986234fd39abb7e7c497e71677fdc3c1678
Ildiko Czeller <czeildi@gmail.com>
2026-02-02 22:23:32 +0100

    refactor for testability and fix tests

--- src/components/games/coin-3-piles/bot-strategy.js
+++ src/components/games/coin-3-piles/bot-strategy.js
@@ -1,29 +1,30 @@
 import { findIndex } from 'lodash';
 
 export const aiBotStrategy = ({ board, moves }) => {
-  const oddPiles = [1, 2, 3].filter(i => board[i - 1] % 2 === 1);
-
-  if (oddPiles.length === 3 || oddPiles.length === 0) {
-    const valueToRemove = findIndex(board, i => i > 0) + 1;
-    const { nextBoard } = moves.removeCoin(board, valueToRemove);
-    if (valueToRemove !== 1) {
-      setTimeout(() => {
-        moves.addCoin(nextBoard, null);
-      }, 0)
-    }
-  }
-  if (oddPiles.length === 2) {
-    const { nextBoard } = moves.removeCoin(board, oddPiles[1]);
+  const { remove, add } = botMoveParams({ board });
+  const { nextBoard } = moves.removeCoin(board, remove);
+  if (add === undefined) return;
+  if (add === null) {
     setTimeout(() => {
-      moves.addCoin(nextBoard, oddPiles[0]);
-    }, 750)
-  }
-  if (oddPiles.length === 1) {
-    const { nextBoard } = moves.removeCoin(board, oddPiles[0]);
-    if (oddPiles[0] !== 1) {
-      setTimeout(() => {
-        moves.addCoin(nextBoard, null);
-      }, 0)
-    }
+      moves.addCoin(nextBoard, null);
+    }, 0);
+  } else {
+    setTimeout(() => {
+      moves.addCoin(nextBoard, add);
+    }, 750);
   }
 };
+
+export const botMoveParams = ({ board }) => {
+  const oddPiles = [1, 2, 3].filter(i => board[i - 1] % 2 === 1);
+
+  if (oddPiles.length === 2) {
+    return { remove: oddPiles[1], add: oddPiles[0] };
+  }
+
+  const valueToRemove = oddPiles.length === 1
+    ? oddPiles[0]
+    : (findIndex(board, i => i > 0) + 1);
+
+  return { remove: valueToRemove, add: valueToRemove !== 1 ? null : undefined };
+}
