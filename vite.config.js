fecf4a245503fc693d0ad57a18a95e5c40fdf203
Ildiko Czeller <czeildi@gmail.com>
2026-02-09 16:20:23 +0100

    define manual chunks to avoid single large chunk

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [
+    react()
+    // visualizer({ open: true })
+  ],
   base: command === 'build' ? '/durer-jatekok/' : '/',
+  build: {
+    rollupOptions: {
+      output: {
+        // Define manual chunks to keep each chunk under the recommended 500kb
+        manualChunks: (id) => {
+          if (id.includes('node_modules/react')) {
+            return 'react-vendor';
+          }
+          if (id.includes('node_modules/lodash')) {
+            return 'lodash';
+          }
+          // has a big bot-strategy file
+          if (id.includes('remove-divisor-multiple')) {
+            return 'remove-divisor-multiple';
+          }
+          // has a relatively big svg that should only be loaded if necessary
+          if (id.includes('shark-chase')) {
+            return 'shark-chase';
+          }
+        }
+      }
+    }
+  },
   esbuild: {
     include: /\.js$/,
     exclude: [],
