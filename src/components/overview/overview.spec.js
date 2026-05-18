11dda524491a55b2a5db3a77b933b8a89e4e1d59
Ildiko Czeller <czeildi@gmail.com>
2026-05-18 12:35:39 +0000

    mock gameList in tests for speedup

--- src/components/overview/overview.spec.js
+++ src/components/overview/overview.spec.js
@@ -4,6 +4,14 @@ import { Overview } from './overview';
 import { gameList } from '../games/gameList';
 import { HashRouter } from 'react-router';
 
+vi.mock('../games/gameList', () => ({
+  gameList: {
+    GameA: { name: { hu: 'A játék' }, category: ['A'], year: { k: 'I. (07/08)', v: '07/08' }, round: 'döntő' },
+    GameB: { name: { hu: 'B játék' }, category: ['B'], year: { k: 'II. (08/09)', v: '08/09' }, round: 'döntő' },
+    GameC: { name: { hu: 'C játék' }, category: ['C'], year: { k: 'III. (09/10)', v: '09/10' }, round: 'online' }
+  }
+}));
+
 describe('Overview', () => {
   it('should show a list of available games', async () => {
     const { container } = render(<HashRouter><Overview /></HashRouter>);
